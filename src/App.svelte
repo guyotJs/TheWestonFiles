01fa436a311c4fad4c255bc18897f5fc8606532a
Weston <classicmclive@gmail.com>
2023-03-10 14:12:18 -0500

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,44 @@
+<style>
+  h1{border-bottom: 1px solid black;font-weight: 10;font-size:30px;}
+  src{font-weight: 10;font-size:26px;}
+  main{margin-left:10%;margin-right:10%;} 
+  img{width:200px!important;height:263px!important;}
+  img{float: right;}
+  .fix{margin-right:230px; }
+  p{margin-right: 15px;}
+  a{color:rgb(0, 123, 255);}
+  r{color:rgb(255, 0, 0);}
+  cc{color:rgb(0, 183, 255)}
+  top{font-weight: 10;font-size:30px;margin:5%;}
+  topSub{margin:5%;}
+  articleExtra{
+    float: right;
+    margin-right: 5%;
+    padding-top:10px;
+  }
+</style>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<articleExtra><p align="center">This article was translated<br/> from <b>Swedish</b> </p></articleExtra>
+<top><em>TheWestonFiles</em></top><br/>
+<topSub>The <b>Wikipedia</b> for <em>Nerds</em></topSub>
+<br/><br/>
+<main>
+  <h1>Pehr Högström</h1><br/>
+  <p align="right"><img class="imgr bc-border" src="https://upload.wikimedia.org/wikipedia/commons/5/5b/Pehr_Hogstrom.jpg"/></p>
+  <p class="">
+    <!--Intro-->
+    Pehr Högström born November 10, 1714 in <a>Selånger</a> in the province of <a>Medelpad</a>, died July 14, 1784 in <a>Skellefteå</a>, and was a Swedish priest and writer. He helped to shape the Southern Lapland literary language that came to dominate the literature published in <a>Sámi</a> in Sweden at the end of the 18th century and the 19th century.
+  </p>
+  <h1 class="fix">Biography</h1><br/>
+  <!--Bio 1-->
+  <p class="">Högström became a student in <a>Uppsala</a> in 1733, where he was promoted to <a>Master of Philosophy</a> in 1743. Two years earlier he was ordained a priest in <a>Stockholm</a> and he became a <a>missionary</a> in <a>Lapland</a>, where he ministered in the areas around the <a>Kaitum River</a>. In 1742 he was appointed vicar of the newly established <a>parish</a>, which was named <a>Gällivare</a>. </p>
+  <br/>
+  <!--Bio 2-->
+  <p>In 1743 and 1744, Högström participated in the conferences aimed at developing a standardized written language for the <a>Sámi</a> initiated by vicar <r>Pehr Fjellström</r> in the <a>Lycksele</a> parish. The result of these conferences was the <a>Southern Lapland</a> book language. Högström further developed this written language by taking greater account of the <a>Sámi</a> spoken in the far north. His variant of the <a>Southern Lapland</a> literary language became the official literary language and was used in the translation and revision of <a>Sámi</a> books for a long period of time. Among other things, the bible was published in this language in 1811.<cc>[1]</cc></p><br/>
+  <!--Bio 3-->
+  <p>During his extensive official travels, partly in his congregation, and partly as a visitor, he had good opportunities to study the nature and customs of the <a>Lapplands</a>. Based on these studies, he wrote Description of the Lappmarks subject to Sweden's crown, which was published in 1747, translated into several languages, and led to Högström receiving a place in the <a>Swedish Academy of Sciences</a>. In 1749 he became a parish priest in <a>Skellefteå</a>. He testified in the 1755–1756, 1765–1766, 1769–1770 and 1771–1772 <a>Riksdags</a> and in 1772 became a doctor of <a>theology</a>. Högström made great efforts to enable <a>Sámi</a> people to be educated, both through his own teaching activities but also through his translation of Johann Gabriel Güttner's postilla (1748) into <a>Sámi</a> and authorship of a concise explanation of Luther's catechism in the same year. In the hymn book published in <a>Sámi</a> in 1744, he translated 20 hymns. He also had a great interest in natural science and worked to modernize agriculture in <a>Skellefteå</a>.</p>
+  <!--Sources-->
+  <h1><src>Sources</src></h1><br/>
+  &emsp;1.<a>^</a> Forsgren, Tuuli (1988). <a>Samisk kyrko- och undervisningslitteratur i Sverige 1619-1850. <i class="fa fa-external-link" aria-hidden="true" style="font-size:13px;"></i></a> Forskningsarkivet, Umeå universitet.
+</main>
+<br/><br/>
